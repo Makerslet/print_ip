2836f129df867fe27fe976579974fdaf7ce937b7
Makerslet <oshuev.valery@yandex.ru>
2020-02-18 16:01:41 +0300

    to_string для интегральных типов

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
+#include "to_string.h"
 
 int main (int, char **)
 {
+    char ch = -1;
+    to_string(ch);
+
+    short sh = 0;
+    to_string(sh);
+
+    int i = 2130706433;
+    to_string(i);
+
+    long l = 8875824491850138409;
+    to_string(l);
+
     return 0;
 }
 
---
+++ src/to_string.h
@@ -0,0 +1,38 @@
+#ifndef TO_STRING_H
+#define TO_STRING_H
+
+#include <string>
+#include <iostream>
+#include <type_traits>
+
+template <typename T>
+unsigned char get_byte(const T& value, std::size_t position) {
+
+    std::size_t element_size = sizeof (T);
+    std::size_t shift_size = 8 * ((element_size - 1) - position);
+    T copy = value >> shift_size;
+
+    return  copy & 0xFF;
+}
+
+template <typename T,
+          typename Fake =
+          typename std::enable_if<std::is_integral<T>::value>::type
+          >
+std::string to_string(const T& arg) {
+
+    std::string result;
+    std::size_t element_size = sizeof (T);
+
+    for(std::size_t i = 0; i < element_size; ++i) {
+
+        result += std::to_string(get_byte(arg, i));
+        if(i < (element_size - 1))
+            result.push_back('.');
+    }
+
+    std::cout << "result: " << result << std::endl;
+    return result;
+}
+
+#endif // TO_STRING_H
